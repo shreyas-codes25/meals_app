c5a66a0283a2735f4181ec7fec9f22738954105d
shreyas-codes25 <140911851+shreyas-codes25@users.noreply.github.com>
2024-06-15 20:43:49 +0530

    asdasdas

--- lib/app.dart
+++ lib/app.dart
@@ -1,1 +1,15 @@
 import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Meals App',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(),
+      theme: theme,
+    );
+  }
+}
